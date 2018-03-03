d8154aa43eacf75ce2cb292a85b53fec89da3883
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-03 23:59:58 +1100

    Update app.js to use best practice

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const R = require('ramda')
 
 exports.handler = function(event, context, callback) {
+  const response = buildGreeting(event.queryStringParameters)
+  callback(null, response)
+}
+
+// Best practice to separate functions from handler
+const buildGreeting = params => {
   const greet = R.replace('{name}', R.__, 'Hello, {name}!')
-  const name = !!event.queryStringParameters && event.queryStringParameters.name
+  const name = !!params && params.name
   const message = !!name ? greet(name) : greet('World')
   const payload = {
     data: {
       message: message
     }
   }
-
-  const response = {
+  return {
     statusCode: 200,
     body: JSON.stringify(payload)
   }
-
-  callback(null, response)
 }
